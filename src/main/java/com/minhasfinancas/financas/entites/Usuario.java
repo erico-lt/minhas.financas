6561aeed6acfd214a7fb778141a6a30ef98d1239
Erico Santana <erico.blp@gmail.com>
2023-07-29 03:08:32 -0300

    criando classe Usuario

---
+++ src/main/java/com/minhasfinancas/financas/entites/Usuario.java
@@ -0,0 +1,103 @@
+package com.minhasfinancas.financas.entites;
+
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario", schema = "financas")
+public class Usuario {
+    
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String senha;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "GMT")
+    private Instant data_cadastro; 
+
+    public Usuario() {        
+    }     
+
+    public Usuario(Long id, String name, String email, String senha, Instant data_cadastro) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.senha = senha;
+        this.data_cadastro = data_cadastro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Instant getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(Instant data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }        
+
+}
