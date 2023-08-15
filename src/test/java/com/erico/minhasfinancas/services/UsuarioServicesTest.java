049111e021ab06b63df469f973a9cf2066461ac9
Erico Santana <erico.blp@gmail.com>
2023-08-14 21:17:07 -0300

    finaliizando testes validar usuario

--- src/test/java/com/erico/minhasfinancas/services/UsuarioServicesTest.java
+++ src/test/java/com/erico/minhasfinancas/services/UsuarioServicesTest.java
@@ -19,82 +19,77 @@ import com.erico.minhasfinancas.exceptions.RegraNegocioException;
 import com.erico.minhasfinancas.repositorys.UsuarioRepository;
 import com.erico.minhasfinancas.services.impl.UsuarioServicesImpl;
 
-
 @ActiveProfiles("test")
 @SpringJUnitConfig
 public class UsuarioServicesTest {
-    
- 
-    UsuarioServices usuarioServices; 
+
+    UsuarioServices usuarioServices;
 
     @MockBean
     UsuarioRepository usuarioRepository;
 
+    Usuario usuario;
+
     @BeforeEach
-    void setup() {        
-        usuarioServices = new UsuarioServicesImpl(usuarioRepository);
+    void setup() {
+        usuarioServices = new UsuarioServicesImpl(usuarioRepository);        
+        usuario = Usuario.builder().email("email@email.com").senha("12345").build();
     }
 
     @Test
     void testeDeveRetornarUmUsuarioQueFoiSalvoNoBanco() {
         assertDoesNotThrow(() -> {
-            String email = "email@email.com";
-            String senha = "12345";
-            Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
 
-            Mockito.when(usuarioRepository.findByEmail(email)).thenReturn(Optional.of(usuario));
+            Mockito.when(usuarioRepository.findByEmail("email@email.com")).thenReturn(Optional.of(usuario));
 
-            Usuario result = usuarioServices.autenticar(email, senha);
+            Usuario result = usuarioServices.autenticar("email@email.com", "12345");
 
             Assertions.assertThat(result).isNotNull();
         });
     }
 
     @Test
-    void testeDeveRetornarUmaExcecaoSeAutenticacaoFalharQuandoEmailNaoEstiverCorreto() {
-        assertThrows(ErroAutenticacaoException.class, () -> {
-            String email = "email@email.com";
-            String senha = "12345";
-            Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+    void testeDeveRetornarUmaExcecaoSeAutenticacaoNaoEncontrarUsuarioComEmailInformado() {
+        ErroAutenticacaoException e = assertThrows(ErroAutenticacaoException.class, () -> {            
+            Mockito.when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.of(usuario));
 
-            Mockito.when(usuarioRepository.findByEmail(email)).thenReturn(Optional.of(usuario));
-
-            usuarioServices.autenticar("2email@email.com", senha);            
+            usuarioServices.autenticar("2email@email.com", usuario.getSenha());
         });
+
+        Assertions.assertThat(e.getMessage()).isEqualTo("Usuario não encontrado, verifique o email");
     }
 
     @Test
     void testeDeveRetornarUmaExcecaoSeAutenticacaoFalharQuandoSenhaNaoEstiverCorreto() {
-        assertThrows(ErroAutenticacaoException.class, () -> {
-            String email = "email@email.com";
-            String senha = "12345";
-            Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+        ErroAutenticacaoException e = assertThrows(ErroAutenticacaoException.class, () -> {            
 
-            Mockito.when(usuarioRepository.findByEmail(email)).thenReturn(Optional.of(usuario));
+            Mockito.when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(Optional.of(usuario));
 
-            usuarioServices.autenticar(email, "1234");            
+            usuarioServices.autenticar(usuario.getEmail(), "1234");
         });
+
+        Assertions.assertThat(e.getMessage()).isEqualTo("Senha incorreta");
     }
 
     @Test
-    void validarEmailNãoDeveContemEmailNoBanco() {        
+    void validarEmailNãoDeveContemEmailNoBanco() {
 
         assertDoesNotThrow(() -> {
             Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
 
             usuarioServices.validarEmail("email@email.com");
         });
-       
-    } 
+
+    }
 
     @Test
     void deveLancarErroComEmailJaExistenteNoBanco() {
-        assertThrows(RegraNegocioException.class, () -> {            
+        assertThrows(RegraNegocioException.class, () -> {
 
             Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
             usuarioServices.validarEmail("email@email.com");
         });
-    } 
-   
+    }
+
 }
